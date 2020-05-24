ef8789cd4a2b1cfd2a25053a4d09bc2d97ef040f
thisoban <thisoban_1995@hotmail.com>
2020-05-24 13:58:17 +0200

    logic two methods added

---
+++ Website/WebApplication1/LogicLayer/UserLogic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer
+{
+    public class UserLogic
+    {
+        private readonly UserLogic dal = new UserLogic();
+        public bool LoginUser(string username, string password)
+        {
+           
+            bool succesful = false;
+            if (dal.LoginUser()) 
+
+        }
+
+        public bool RegisterUser()
+        {
+            bool succesful = false;
+
+        }
+    }
+}
