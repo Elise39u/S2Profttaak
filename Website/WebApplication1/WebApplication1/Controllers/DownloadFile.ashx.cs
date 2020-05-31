86dc4b99c675bc05337730e941f58d4d2f2b96ef
justinLaar322 <justin555@live.nl>
2020-05-31 12:59:48 +0200

    Fixed the issue with the Download handler

--- Website/WebApplication1/WebApplication1/Controllers/DownloadFile.ashx.cs
+++ Website/WebApplication1/WebApplication1/Controllers/DownloadFile.ashx.cs
@@ -1,28 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Web;
 
 namespace WebApplication1.Controllers
 {
-    public class Download : IHttpHandler
+    /// <summary>
+    /// Summary description for DownloadFile
+    /// </summary>
+    public class DownloadFile : IHttpHandler
     {
-        /// <summary>
-        /// You will need to configure this handler in the Web.config file of your 
-        /// web and register it with IIS before being able to use it. For more information
-        /// see the following link: https://go.microsoft.com/?linkid=8101007
-        /// </summary>
-        #region IHttpHandler Members
-
-        public bool IsReusable
-        {
-            // Return false in case your Managed Handler cannot be reused for another request.
-            // Usually this would be false in case you have some state information preserved per request.
-            get { return true; }
-        }
-
         public void ProcessRequest(HttpContext context)
         {
-            //write your handler implementation here.
             string file = context.Request.QueryString["file"];
 
             if (!string.IsNullOrEmpty(file) && File.Exists(context.Server.MapPath(file)))
@@ -45,6 +35,12 @@ namespace WebApplication1.Controllers
             }
         }
 
-        #endregion
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
     }
 }
